79a4320bbbadaa8df9c347fa809c2c3695f98dbc
Marcin Lawniczak <marcin@lawniczak.me>
2021-03-16 22:50:40 +0100

    Add 2021-03 empty files

---
+++ 2021-03-16/zadanie_03_snooper.c
@@ -0,0 +1,6 @@
+/**
+ * Napisz program, który nasłuchuje ramki sieciowe ze wskazanego jako pierwszy argument
+ * wywołania interfejsu sieciowego i wy´swietla nast˛epuj ˛ace informacje o ka˙zdej odebranej ramce:
+ * jej rozmiar, adres MAC nadawcy, adres MAC odbiorcy, typ pakietu (warto´s´c pola sll_pkttype
+ * struktury sockaddr_ll), warto´s´c EtherType oraz transmitowane w niej dane.
+**/
---
+++ 2021-03-16/zadanie_04_repeater.c
@@ -0,0 +1,9 @@
+/**
+ * Napisz program, który działa zgodnie z nastepujaca zasada:
+ * • program odbiera ramka sieci Ethernet II z interfejsu sieciowego, którego nazwa podawana
+ * jest jako jego pierwszy argument wywołania;
+ * • program, odebrane z tej ramki dane, przesyła do kolejnego odbiorcy, korzystajac z tego samego interfejsu sieciowego,
+ * pod adres MAC, który podawany jest jako jego drugi argument wywołania;
+ * • program powraca do odbierania kolejnej ramki.
+ * Jako wartosci EtherType uzyj liczby 0x8888.
+**/
