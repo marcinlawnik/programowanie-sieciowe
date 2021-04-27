0585d6410f177df7b48cf477714280e840b0a9f9
Marcin Lawniczak <marcin@lawniczak.me>
2021-04-27 23:07:16 +0200

    Add zad 7

--- 2021-03-02/udp-server.c
+++ 2021-03-02/udp-server.c
@@ -1,30 +1,29 @@
-#include <netdb.h>
+/*
+ * Copyright (C) 2021 Michal Kalewski <mkalewski at cs.put.poznan.pl>
+ *
+ * Compilation:  gcc -Wall ./udp-server.c -o ./udp-server
+ * Usage:        ./udp-server
+ *
+ * Bug reports:  https://gitlab.cs.put.poznan.pl/mkalewski/ps-2021/issues
+ *
+ */
+
 #include <netinet/in.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
-#define LISTEN_PORT     1111
-
 int main(int argc, char** argv) {
     int sfd, rc;
     socklen_t sl;
     char buf[128];
-    struct sockaddr_in saddr, caddr, naddr;
-    struct hostent* addrent;
-
-    addrent = gethostbyname(argv[1]);
-
-    memset(&naddr, 0, sizeof(naddr));
-    naddr.sin_family = AF_INET;
-    memcpy(&naddr.sin_addr.s_addr, addrent->h_addr, addrent->h_length);
-    naddr.sin_port = htons(atoi(argv[2]));
+    struct sockaddr_in saddr, caddr;
 
     memset(&saddr, 0, sizeof(saddr));
     saddr.sin_family = AF_INET;
     saddr.sin_addr.s_addr = INADDR_ANY;
-    saddr.sin_port = htons(LISTEN_PORT);
+    saddr.sin_port = htons(1234);
     sfd = socket(PF_INET, SOCK_DGRAM, 0);
     bind(sfd, (struct sockaddr*) &saddr, sizeof(saddr));
     while(1) {
@@ -32,10 +31,8 @@ int main(int argc, char** argv) {
         memset(&buf, 0, sizeof(buf));
         sl = sizeof(caddr);
         rc = recvfrom(sfd, buf, 128, 0, (struct sockaddr*) &caddr, &sl);
-        sendto(sfd, buf, rc, 0, (struct sockaddr*) &naddr, sl);
+        sendto(sfd, buf, rc, 0, (struct sockaddr*) &caddr, sl);
     }
     close(sfd);
     return EXIT_SUCCESS;
 }
-
-
