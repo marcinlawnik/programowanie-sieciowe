4e0d4c89f4129793003ba00423a6ef07fc0c5841
Marcin Lawniczak <marcin@lawniczak.me>
2021-05-11 17:36:02 +0200

    Add more code

--- 2021-04-27/zadanie_04_ipforward.c
+++ 2021-04-27/zadanie_04_ipforward.c
@@ -34,9 +34,9 @@ int main(int argc, char **argv) {
     memset(&addr, 0, sizeof(addr));
     sl = sizeof(addr);
 
+    rc = recvfrom(sfd, buf, sizeof(buf), 0, (struct sockaddr *)&addr, &sl);
+    ip = (struct iphdr *)&buf;
     while (1) {
-        rc = recvfrom(sfd, buf, sizeof(buf), 0, (struct sockaddr *)&addr, &sl);
-        ip = (struct iphdr *)&buf;
         if (ip->protocol == IPPROTO_CUSTOM) {
             inet_ntop(AF_INET, &ip->saddr, (char *)&saddr, 16);
             inet_ntop(AF_INET, &ip->daddr, (char *)&daddr, 16);
@@ -46,7 +46,7 @@ int main(int argc, char **argv) {
             memset(&next_addr, 0, sizeof(next_addr));
             next_addr.sin_family = AF_INET;
             next_addr.sin_port = 0;
-            next_addr.sin_addr.s_addr = inet_addr(argv[1]);
+            next_addr.sin_addr.s_addr = inet_addr(argv[0]);
             sendto(next_sock, buf, strlen(buf), 0, (struct sockaddr *)&next_addr,
                    sizeof(next_addr));
             close(next_sock);
