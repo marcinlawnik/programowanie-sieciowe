6b36f3371950a422dc965a21c81b44b319cbe42f
Marcin Lawniczak <marcin@lawniczak.me>
2021-05-18 17:26:37 +0200

    Add code

--- 2021-05-11/zadanie_04_client_ipv4-and-6.c
+++ 2021-05-11/zadanie_04_client_ipv4-and-6.c
@@ -15,6 +15,26 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+int _connect(const char *host, const char *service) {
+    int sfd;
+    struct addrinfo hints, *res, *ressave;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    getaddrinfo(host, service, &hints, &res);
+    ressave = res;
+    do {
+        sfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+        if (connect(sfd, res->ai_addr, res->ai_addrlen) == 0)
+            break;
+        close(sfd);
+    } while ((res = res->ai_next) != NULL);
+    freeaddrinfo(ressave);
+    return sfd;
+}
 
 int main(int argc, char** argv) {
     int sfd, rc;
@@ -26,7 +46,7 @@ int main(int argc, char** argv) {
     saddr.sin6_family = AF_INET6;
     saddr.sin6_port = htons(atoi(argv[2]));
     inet_pton(AF_INET6, argv[1], &saddr.sin6_addr);
-    connect(sfd, (struct sockaddr*) &saddr, sizeof(saddr));
+    _connect(sfd, (struct sockaddr*) &saddr, sizeof(saddr));
     rc = read(sfd, buf, 128);
     write(1, buf, rc);
     close(sfd);
